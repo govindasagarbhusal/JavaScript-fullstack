bf1fd405d2cdefca2d3c8d3a8ab2d4be7cdeed20
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2025-06-23 22:55:41 +0545

    variables

---
+++ 01.Variables/Variable.js
@@ -0,0 +1,38 @@
+// How to declare a variable
+// (Reserved Keyword) (Variable Name) (type of value)
+
+// Declared Variable
+// let name;
+
+// Assign Value 
+// let banana;
+// banana = "banana";
+// console.log(banana);
+
+// let vat = 'variable'
+// console.log(vat);
+
+// let 10aayush = "Govinda" --> this gives an error because the first letter of a variable cannot be a number
+// let aayush10 = "Govinda" --> this works
+
+// let aay-ush = "Govinda" --> this gives an error because compiler reads the '-' and a minus not a dash
+
+// const youtubeChannel = "JJOlatunji"
+// youtubeChannel = "KSIMON" --> const(constant) variables cannot be changed again
+// console.log(youtubeChannel);
+
+// let name = "Govinda"
+// let whatDoYouWannaBe = "developer"
+// let gender = "male"
+// let instaHandle = "lazy.elephant_"
+// console.log(name);
+// console.log(name, whatDoYouWannaBe, gender, instaHandle);
+// console.log(whatDoYouWannaBe);
+// console.log(gender);
+// console.log(instaHandle);
+
+//                                           TYPES OF VARIABLES
+//                                             primitive type
+// let num = 10.1
+// console.log(typeof num);
+
---
+++ 02.Numbers/numbers.js
@@ -0,0 +1,23 @@
+// basic maths
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+// INCREMENT OPREATOR
+// let counter = 0;
+// counter++ --> 1
+// counter++ --> 2
+// counter++ --> 3
+// counter++ --> 4
+// console.log(counter);
+
+// DECREMENT OPREATOR
+// let counter = 10;
+// counter-- --> 9
+// counter-- --> 8
+// counter-- --> 7
+// counter-- --> 6
+// console.log(counter);
