b5a66443b88a53318716d8ec818a333f1baaa1cc
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2025-06-25 17:26:23 +0545

    booleans

---
+++ 03.Booleans/booleans.js
@@ -0,0 +1,28 @@
+// Boolean --> either true or false
+
+// let isTrue = false;
+// console.log(isTrue);
+
+// let number = 10;
+// console.log(number + undefined);
+
+// --> falsy values
+    
+
+    // -> false  
+    // -> null  
+    // -> undefined  
+    // -> 0  
+    // -> -0  
+    // -> NaN  
+    // -> '', "", ``, (empty quotes)
+    
+// let notDefined = ''
+// console.log(notDefined);
+
+// let isProgrammingLanguage = true
+// let isJsHard = false
+// let favNum = 23
+// console.log(isProgrammingLanguage);
+// console.log(isJsHard);
+// console.log(favNum + undefined);
