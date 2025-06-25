a137f5c96c406c35d50769f576d50a36b54fdeef
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2025-06-25 17:46:13 +0545

    comparision operators

---
+++ 04.Compasition Operator/comparision.js
@@ -0,0 +1,44 @@
+// Comparision Operators
+
+
+// -> RELATIONAL OPERATORS
+//   > Greater Then
+//   < Less Then
+//   >= Greater Then or equal to
+//   >= Less Then or equal to
+
+// console.log(10 > 10); -> False
+// console.log(10 < 10); -> False
+// console.log(10 <= 10); -> True
+// console.log(10 >= 10); -> True
+
+
+
+// -> EQUALITY OPERATORS
+// === strict equality --> (type + value)
+// !== strict non-equality --> (type + value)
+// == lose equality operator --> (value)
+// != lose not equality Operator --> (value)
+
+// console.log(10 === 10); -->  true (same value)
+// console.log(10 === 20); -->  false (different value)
+// console.log(10 === '10');  --> false (same value | different type)
+// console.log('20' === '10');  --> false (same type | different value)
+
+// console.log(10 == 10); -->  true (same value)
+// console.log(10 == 20); -->  false (different value)
+// console.log(10 == '10');  --> false (same value)
+// console.log('20' == '10');  --> false (different value)
+
+// *********************************TASK********************************************
+
+let firstFavNum = 23
+let secondFavNum = 24
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum != secondFavNum);
