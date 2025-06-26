52ce49e3cbda52d35c569bae0f75a9875aaafbbd
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2025-06-26 21:28:21 +0545

    just for the streak

---
+++ 05.Strings/Strings.js
@@ -0,0 +1,33 @@
+//  STRINGS
+let firstName = "Govinda "
+let secondName = ""
+let lastName = "Bhusal"
+// console.log(firstName);
+// console.log(secondName);
+// console.log(lastName);
+
+// 1. STRING CONCATNATION
+// console.log(firstName + secondName + lastName);
+// let fullName = firstName.concat(secondName)
+
+// 2. APPEND
+// firstName += "somehting else"
+// console.log(firstName);
+
+// 3. LENGTH
+// console.log(firstName.length);
+
+// 4. CASES
+// console.log(firstName.toUpperCase()); 
+// console.log(firstName.toLowerCase()); 
+
+// 5. SLICE
+// console.log(firstName.slice(0, 3));
+
+// 6. SPLIT
+// console.log(firstName.split(' ').join(' '));
+
+// 7. INCLUDES
+console.log(firstName.includes());
+
+// just for the streak
