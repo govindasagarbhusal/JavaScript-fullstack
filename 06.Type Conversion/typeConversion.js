c508a025bf28ba9356f34e9f85b20ba7a2ac5425
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2025-07-01 08:30:51 +0545

    type Conversion

---
+++ 06.Type Conversion/typeConversion.js
@@ -0,0 +1,19 @@
+// TYPE CONVERSION
+
+// --- Convert string to number ---
+// let money = "50"
+// money = parseInt(money)
+// money = +money
+// money = Number(money)
+
+// -- Convet number to string
+// let money = 50
+// money = money.toString()
+// money = String(money)
+
+// --- Convert string to decimal
+// let money = '50.245'
+// money = parseFloat(money)
+
+// console.log(money);
+// console.log(typeof money);
