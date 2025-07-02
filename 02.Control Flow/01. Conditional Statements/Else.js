5b1f55d73e82254b90396a3044f5e618e4ee8967
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2025-07-02 21:42:57 +0545

    switch statements

---
+++ 02.Control Flow/01. Conditional Statements/Else.js
@@ -0,0 +1,42 @@
+// else conditions
+
+// let a = 20
+// let b = 20
+
+// if (a > b) {
+//     console.log("a is greater than b");
+// } else if (a < b){
+//     console.log("a is less than b");
+// } else {
+//     console.log("a is equal to b");
+// }
+
+// let time = 29
+// let greetings;
+
+// if (time < 10) {
+//     greetings = "Good Morning"
+//     console.log(greetings);
+// } else if (time < 20) {
+//     greetings = "Good evening"
+//     console.log(greetings);
+// } else {
+//     greetings = "Good Night"
+//     console.log(greetings);
+// }
+
+// ***************************************** EXERCISE *******************************************   
+
+let password = 10
+;
+
+if (password === 8){
+    console.log("welcome");
+} else if( password <= 8){
+    console.log("Password is too short");
+} else if(password >= 8){
+    console.log("Passowrd is too long");
+    console.log("Passowrd must be 8 characters");
+} else {
+    console.log("please provide a password");
+}
---
+++ 02.Control Flow/02.Switch Statements/switch.js
@@ -0,0 +1,68 @@
+// Switch statements
+
+// let x = 0;
+// let bulb;
+
+// switch (x) {
+//     case 0:
+//         bulb = "off"
+//         console.log(bulb);
+//         break;
+
+//     case 1:
+//         bulb = "on";
+//         console.log(bulb);
+//         break;
+
+//     default:
+//         bulb = "no value found";
+//         console.log(bulb);
+//         break;
+// }
+
+// let day = "monday"
+
+// switch (day) {
+//     case "monday":
+//         console.log("today is monday");
+//         break;
+//     case "tuesday":
+//         console.log("today is tuesday");
+//         break;
+//     case "wednesday":
+//         console.log("today is wednesday");
+//         break;
+//     case "thursday":
+//         console.log("today is thursday");
+//         break;
+//     case "friday":
+//         console.log("today is friday");
+//         break;
+//     case "saturday":
+//         console.log("today is saturday");
+//         break;
+//     case "sunday":
+//         console.log("today is sunday");
+//         break;
+//     default:
+//         console.log("Day is unknown");
+// }
+
+let fruit = "banana"
+
+switch (fruit) {
+    case "banana":
+    console.log("banana is good");
+    break;
+
+    case "oranges":
+    console.log("i dont really like oranges");
+    break;
+
+    case "apples":
+        console.log("How do you like them apples");
+        break;
+
+    default:
+        console.log("I have never heard of that fruit");
+}
